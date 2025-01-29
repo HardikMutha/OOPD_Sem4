c7606d185aaff63fbd43a290c7252bdc8aed2779
HardikMutha <hardikmutha2005@gmail.com>
2025-01-29 22:09:32 +0530

    Initial Commit

---
+++ newprog.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class newprog {
+  public static void main(String[] args) {
+    Integer a = 12412;
+    Integer b = 12412;
+    System.out.println(a.equals(b));
+    // Integer a[] = new Integer[16];
+    // for(int i=0;i<16;i++)
+    //    System.out.println(a[i]);
+    List<String> l1 = new ArrayList<>();
+    l1.add("Hello");
+    l1.add("Nice");
+    System.out.println(l1.get(0));
+    return;
+  }
+}
---
+++ prog.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class prog {
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number");
+    int k = sc.nextInt();
+    System.out.println(k);
+    int a = 12;
+    String s = "Hardik";
+    char ch = s.charAt(2);
+    System.out.println(ch);
+    System.out.println("Nice");
+    System.out.println("Hello World");
+  }
+}
+
